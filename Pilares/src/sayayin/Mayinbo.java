c8d230d1497d88badb4b6e7ecfbd2604a9d1558a
Giancarlo Valencia Sotomayor <gvalencia@vallegrande.edu.pe>
2022-11-15 17:35:05 -0500

    ok

---
+++ Pilares/src/sayayin/Mayinbo.java
@@ -0,0 +1,16 @@
+package herores;
+
+import java.util.Date;
+
+public class Mayinbo {
+        String colorCabello;
+    int edad;
+    String nombre;
+    double altura;
+    int nivelKit;
+    double peso;
+    String raza;
+    String origen;
+    Date regeneracion; 
+    int flexibilidad;
+}
---
+++ Pilares/src/sayayin/Picolo.java
@@ -0,0 +1,19 @@
+
+package herores;
+
+import java.util.Date;
+
+
+public class Picolo {
+
+    String colorCabello;
+    int edad;
+    String nombre;
+    double altura;
+    int nivelKit;
+    double peso;
+    String raza;
+    String origen;
+    Date regeneracion;
+    int flexibilidad;
+}
